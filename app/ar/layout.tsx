bcfd88089fc91ed23d795cf84dcd3cd5ac8bca48
Shiftadv1 <shift.sa00@gmail.com>
2026-05-09 23:16:21 +0300

    Update layout.tsx

--- app/ar/layout.tsx
+++ app/ar/layout.tsx
@@ -3,3 +3,11 @@ export const metadata = {
   description:
     "وكالة شيفت للدعاية والإعلان في الرياض تقدم خدمات الهوية التجارية والطباعة والتصوير وتصميم البوثات واليونيفورم.",
 }
+
+export default function ArabicLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <>{children}</>
+}
